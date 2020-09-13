c3795a3da42911dea3e87549b9802beb694803af
wire2coder <wire2coder@git.com>
2020-09-13 01:07:02 -0400

    check point, added getMetrics(), program compiled and runs

--- src/com/pack/a/Home2.scala
+++ src/com/pack/a/Home2.scala
@@ -5,8 +5,19 @@ import org.apache.spark.SparkContext._
 import org.apache.log4j._
 import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.regression._
+import org.apache.spark.mllib.evaluation
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.tree._
+import org.apache.spark.mllib.tree.model._
+import org.apache.spark.rdd._
+
 
 object Home2 {
+
+  def getMetrics(model: DecisionTreeModel, data: RDD[LabeledPoint] ): MulticlassMetrics = {
+    val predictionsAndLabels = data.map( example => ( model.predict(example.features), example.label) )
+    new MulticlassMetrics(predictionsAndLabels)
+  }
 
   def main( args: Array[String] ) {
 
@@ -20,16 +31,31 @@ object Home2 {
     val rawData = sc.textFile("./covtype.data")
 //    rawData.foreach(println)
 
+    // making the 'vector of feature'
     val data = rawData.map{ line =>
       val values = line.split(",").map( asdf => asdf.toDouble)
-      val featureVector = Vectors.dense(values.init)
-      val label = values.last - 1
-      LabeledPoint(label, featureVector)
 
+      // .init does not gives you the 'last column'
+      val featureVector = Vectors.dense(values.init)
+
+      // value of the "last column" subtract 1
+      val label = values.last - 1
+
+      // (4.0,[2596.0,51.0,3.0,258.0,0.0,510.0,221.0,232.0,148.0,6279.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+      LabeledPoint(label, featureVector)
     }
 
-    val jim1 = data.take(20)
-    jim1.foreach(println)
+    val Array(trainData, cvData, testData) = data.randomSplit( Array(0.8, 0.1, 0.1) )
+
+    trainData.cache()
+    cvData.cache()
+    testData.cache()
+
+    val model = DecisionTree.trainClassifier( trainData, 7, Map[Int, Int](), "gini", 4, 100)
+    val metrics = getMetrics(model, cvData)
+
+//    val jim1 = data.take(20)
+//    jim1.foreach(println)
 
 
     println(" ")
