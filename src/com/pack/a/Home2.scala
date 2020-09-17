fdceebd46bd2859da393326dcb657bf97a82d6a6
wire2coder <@t0mix1213>
2020-09-17 08:14:57 -0400

    Got it to show something, but println(metircs.precision) line 47 still throws error

--- src/com/pack/a/Home2.scala
+++ src/com/pack/a/Home2.scala
@@ -1,10 +1,13 @@
 package com.pack.a
 
-import org.apache.spark._
-import org.apache.spark.SparkContext._
-import org.apache.log4j._
-import org.apache.spark.mllib.linalg._
-import org.apache.spark.mllib.regression._
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.{DecisionTree, RandomForest}
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Home2 {
 
@@ -14,27 +17,61 @@ object Home2 {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
     // make new 'sc' object thing
-    val sc = new SparkContext( new SparkConf().setAppName("home work 2").setMaster("local") )
+    val sc = new SparkContext( new SparkConf().setAppName("RDF").setMaster("local") )
 
     // read the file
     val rawData = sc.textFile("./covtype.data")
 //    rawData.foreach(println)
 
-    val data = rawData.map{ line =>
-      val values = line.split(",").map( asdf => asdf.toDouble)
+    val data = rawData.map { line =>
+      val values = line.split(',').map(_.toDouble)
       val featureVector = Vectors.dense(values.init)
       val label = values.last - 1
       LabeledPoint(label, featureVector)
-
     }
 
-    val jim1 = data.take(20)
-    jim1.foreach(println)
+    // Split into 80% train, 10% cross validation, 10% test
+    val Array(trainData, cvData, testData) = data.randomSplit(Array(0.8, 0.1, 0.1))
+    trainData.cache()
+    cvData.cache()
+    testData.cache()
 
+
+
+    // Build a simple default DecisionTreeModel and compute precision and recall
+    val model = DecisionTree.trainClassifier(trainData, 7, Map[Int,Int](), "gini", 4, 100)
+
+    val metrics = getMetrics(model, cvData)
+
+    println(metrics.confusionMatrix)
+//    println(metrics.precision) // <<< TODO: why is this line wrong?
+
+    (0 until 7).map(
+      category => (metrics.precision(category), metrics.recall(category))
+    ).foreach(println)
+
+//    simpleDecisionTree(trainData, cvData) // <<< TODO
+//    randomClassifier(trainData, cvData)
+//    evaluate(trainData, cvData, testData)
+//    evaluateCategorical(rawData)
+//    evaluateForest(rawData)
+
+    trainData.unpersist()
+    cvData.unpersist()
+    testData.unpersist()
 
     println(" ")
-    println("program finished running, yay!")
+    println("Main function() finished running, yay!")
+
+  } // def main()
+
+  def getMetrics(model: DecisionTreeModel, data: RDD[LabeledPoint]): MulticlassMetrics = {
+    val predictionsAndLabels = data.map(example =>
+      (model.predict(example.features), example.label)
+    )
+    new MulticlassMetrics(predictionsAndLabels)
+
+  } // def getMetrics()
 
 
-  } // def main
-}
+} // Object Home2
